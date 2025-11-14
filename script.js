919265ac7da0f289282b5004986fa5189c795010
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-11-14 18:57:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 
-let fname=document.querySelector("#fname").value;
-let lname=document.querySelector("#lname").value;
-let phone=document.querySelector("#phone").value;
-let email=document.querySelector("#email").value;
+let fname=document.querySelector("#fname");
+let lname=document.querySelector("#lname");
+let phone=document.querySelector("#phone");
+let email=document.querySelector("#email");
 let btn=document.querySelector("button");
 
 btn.addEventListener("click", function(){
