26892c24b1af6766b6e6c04ed0d1f6bda6309f56
Abhilash Jha <152012438+JHA123ABHIlash@users.noreply.github.com>
2025-11-14 19:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //your JS code here. If required.
 
+
+
 let fname=document.querySelector("#fname");
 let lname=document.querySelector("#lname");
 let phone=document.querySelector("#phone");
@@ -7,6 +9,7 @@ let email=document.querySelector("#email");
 let btn=document.querySelector("button");
 
 btn.addEventListener("click", function(){
-	alert(`First Name:${fname.value}Last Name:${lname.value}Phone Number:${phone.value}Email ID:${email.value}`);
+	// preventDefault();
+	alert(`First Name:${fname.value} Last Name:${lname.value} Phone Number:${phone.value} Email ID:${email.value}`);
 });
 
